27c58edae42582a7debde63322bef3fcd942d662
Aibika <sabyraibika@gmail.com>
2025-03-18 14:41:35 +0600

    create calculator ui and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+
+  function addToDisplay(input) {
+    setInput((prev) => prev + input);
+  }
+
+  function clearDisplay() {
+    setInput('');
+  }
+
+  function calculate() {
+    try {
+      setInput(eval(input));
+    } catch (_e) {
+      setInput('Error');
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='phone'>
+      <input type='text' className='display' value={input} readOnly />
+      <div className='buttons'>
+        <button onClick={clearDisplay} className='button button-grey'>
+          AC
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => addToDisplay('/')}
+          className='button button-yellow button-divide'
+        >
+          /
+        </button>
+        <button onClick={() => addToDisplay('7')} className='button'>
+          7
+        </button>
+        <button onClick={() => addToDisplay('8')} className='button'>
+          8
+        </button>
+        <button onClick={() => addToDisplay('9')} className='button'>
+          9
+        </button>
+        <button
+          onClick={() => addToDisplay('*')}
+          className='button button-yellow'
+        >
+          *
+        </button>
+        <button onClick={() => addToDisplay('4')} className='button'>
+          4
+        </button>
+        <button onClick={() => addToDisplay('5')} className='button'>
+          5
+        </button>
+        <button onClick={() => addToDisplay('6')} className='button'>
+          6
+        </button>
+        <button
+          onClick={() => addToDisplay('-')}
+          className='button button-yellow'
+        >
+          -
+        </button>
+        <button onClick={() => addToDisplay('1')} className='button'>
+          1
+        </button>
+        <button onClick={() => addToDisplay('2')} className='button'>
+          2
+        </button>
+        <button onClick={() => addToDisplay('3')} className='button'>
+          3
+        </button>
+        <button
+          onClick={() => addToDisplay('+')}
+          className='button button-yellow'
+        >
+          +
+        </button>
+        <button onClick={() => addToDisplay('0')} className='button button-lg'>
+          0
+        </button>
+        <button onClick={() => addToDisplay(',')} className='button'>
+          ,
+        </button>
+        <button onClick={calculate} className='button button-yellow'>
+          =
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
